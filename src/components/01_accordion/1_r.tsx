0b4b284eec10151c6e44ec7783f8d00cd9eaa79a
김솔지 <so1gging@gmail.com>
2026-05-11 20:37:38 +0900

    라우트 맵 업데이트: 아코디언 컴포넌트를 추가하고 라우트 구조에 통합하여 새로운 경로 설정

---
+++ src/components/01_accordion/1_r.tsx
@@ -0,0 +1,11 @@
+const Accordion1 = () => {
+  return (
+    <>
+      <h3>
+        #1. React<sub>현재 desc만 렌더링</sub>
+      </h3>
+    </>
+  )
+}
+
+export default Accordion1
---
+++ src/components/01_accordion/cx.ts
@@ -0,0 +1,7 @@
+import classNames from 'classnames/bind'
+import styles from './index.module.scss'
+
+// scss 파일 내 각 선택자에 해시 값을 부여
+const cx = classNames.bind(styles)
+
+export default cx
